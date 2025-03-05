c1002d2bba58b74eb14e8672dfef4b49e165c5f3
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-04 20:14:30 -1000

    Disabled page scrolling while the modal window is open

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ const openBtn = document.querySelector(".open"),
   modalWind = document.querySelector(".modal"),
   closebtn = document.querySelector(".close");
 
-console.log(openBtn, modalWind, closebtn);
-
 function toggleModal() {
   modalWind.classList.toggle("active");
+  if (modalWind.classList.contains("active")) {
+    document.body.style.overflow = "hidden"
+  }
+  document.body.style.overflow = "";
 }
 
 closebtn.addEventListener("click", toggleModal);
