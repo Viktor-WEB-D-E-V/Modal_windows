f9b7fe84a0f72f4c04b283f80fff37fb888f67fd
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-04 20:26:22 -1000

    Close the modal window if the user clicks outside the modal window

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 "use strict";
-const openBtn = document.querySelector(".open"),
-  modalWind = document.querySelector(".modal"),
-  closebtn = document.querySelector(".close");
+const modalWind = document.querySelector(".modal"),
+  modalBtn = document.querySelectorAll("[data-modal]");
 
 function toggleModal() {
   modalWind.classList.toggle("active");
   if (modalWind.classList.contains("active")) {
-    document.body.style.overflow = "hidden"
+    document.body.style.overflow = "hidden";
   }
   document.body.style.overflow = "";
 }
 
-closebtn.addEventListener("click", toggleModal);
-openBtn.addEventListener("click", toggleModal);
+modalBtn.forEach((e) => {
+  e.addEventListener("click", toggleModal);
+});
+
+modalWind.addEventListener("click", (e) => {
+  if (e.target === modalWind) {
+    toggleModal();
+  }
+});
